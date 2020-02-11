7194cc435b0f2e441355351e5d793466e5b3566d
prsnk <persnicketygareth@gmail.com>
2020-02-11 23:18:12 +0000

    add test for basic API call

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React, { useState } from "react";
+import { render } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react-hooks";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import useHttp from "./hooks/http.js";
+
+jest.useFakeTimers();
+
+test("tests API call hook", async () => {
+  let url = process.env.REACT_APP_BEER_API_URL;
+  const { result, waitForNextUpdate } = renderHook(() => {
+    useHttp(url);
+  });
+  await waitForNextUpdate();
+  console.log(result.current);
+
+  //console.log("AFTER: ", result.current);
+  //expect(result.current.data.length).toBe(25);
+  expect(result.current.data.length).toBe(0);
+  //console.log(result.current);
 });
